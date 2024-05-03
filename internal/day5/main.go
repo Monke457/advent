a1e60c7b46ca91ef689247a469c4420dc048d18c
Beth <beth.williams@bluewin.ch>
2024-05-03 14:23:57 +0200

    day 5 part 2

--- internal/day5/main.go
+++ internal/day5/main.go
@@ -2,207 +2,223 @@ package main
 
 import (
 	"advent/internal/pkg/reader"
+	"cmp"
 	"fmt"
+	"math"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-var seeds []int
-var seedRanges map[int][]int
-var soil, fert, water, light, temp, hum, loc map[int][]int
-var maps []*map[int][]int = []*map[int][]int{
-	&soil, &fert, &water, &light, &temp, &hum, &loc,
+type seeds struct {
+	start  int
+	length int
 }
 
+type ranger struct {
+	src    int
+	dest   int
+	length int
+}
+
+type mapper struct {
+	to      string
+	rangers []ranger
+}
+
+var content []string
+var seedVals []string
+var maps map[string]mapper
+
 func main() {
-	//fmt.Println(solveFirstProblem())
+	content = reader.FileToArray("data/day5.txt")
+
+	seedVals = strings.SplitAfter(content[0], ":")
+	seedVals = strings.Split(seedVals[1], " ")
+	seedVals = cleanArray(seedVals)
+
+	maps = parseMaps()
+	maps = parseNegativeRange()
+
+	fmt.Println(solveFirstProblem())
 	fmt.Println(solveSecondProblem())
 }
 
 func solveFirstProblem() int {
-	lines := reader.FileToArray("data/day5.txt")
+	seeds := parseSeeds(seedVals)
 
-	seeds, soil, fert, water, light, temp, hum, loc = normalize(lines)
-
-	location := -1
-	for _, seed := range seeds {
-		l := convert(seed, 0)
-		if location == -1 {
-			location = l
-		} else {
-			location = min(location, l)
-		}
+	location := math.Inf(1)
+	for _, s := range seeds {
+		l, _ := convert(s, 1, "seed")
+		location = min(location, l)
 	}
-	return location
+
+	return int(location)
 }
 
 func solveSecondProblem() int {
-	lines := reader.FileToArray("data/day5.txt")
+	s := parseSeedRanges(seedVals)
 
-	seeds, soil, fert, water, light, temp, hum, loc = normalize(lines)
+	location := math.Inf(1)
+	c := make(chan float64)
+	count := 0
+	for _, seed := range s {
+		count++
+		go func(seed seeds) {
+			c <- getLowestLocation(seed)
+		}(seed)
+	}
 
-	seedRanges = getRanges()
-	fmt.Println(seedRanges)
-	fmt.Println(soil)
-
-	location := -1
-	sp := 0
-	for _, r := range seedRanges {
-		fmt.Println("range", r)
-		for i := r[0]; i < r[1]; i++ {
-			l := convert(i, 0)
-			if location == -1 {
+	for i := 0; i < count; i++ {
+		select {
+		case l := <-c:
+			if l < location {
 				location = l
-			} else {
-				location = min(location, l)
 			}
-			i += soil[sp][2]
 		}
 	}
-	return location
+
+	return int(location)
 }
 
-func convert(n, m int) int {
-	if m >= len(maps) {
-		fmt.Println("converted", n)
-		return n
-	}
-	fmt.Println("converting", n)
-	for _, vals := range *maps[m] {
-		if vals[1] <= n && vals[1]+vals[2] >= n {
-			n += vals[0] - vals[1]
-			m++
-			return convert(n, m)
+func getLowestLocation(seed seeds) float64 {
+	loc := float64(-1)
+
+	remaining := seed.length
+	start := seed.start
+
+	for remaining > 0 {
+		l, jump := convert(start, remaining, "seed")
+		remaining -= jump
+		start += jump
+
+		if loc == -1 || l < loc {
+			loc = l
 		}
 	}
-	m++
-	return convert(n, m)
+	return loc
 }
 
-func normalize(lines []string) (
-	[]int,
-	map[int][]int,
-	map[int][]int,
-	map[int][]int,
-	map[int][]int,
-	map[int][]int,
-	map[int][]int,
-	map[int][]int,
-) {
+func convert(val, jump int, name string) (float64, int) {
+	if len(maps[name].to) == 0 {
+		return float64(val), jump
+	}
+	ranger := maps[name].FindRange(val)
+	if ranger != nil {
+		diff := val - ranger.src
+		newVal := ranger.dest + diff
+		return convert(newVal, min(jump, ranger.length-diff), maps[name].to)
+	}
+	return convert(val, 1, maps[name].to)
+}
 
-	var toSoil map[int][]int = map[int][]int{}
-	var toFertilizer map[int][]int = map[int][]int{}
-	var toWater map[int][]int = map[int][]int{}
-	var toLight map[int][]int = map[int][]int{}
-	var toTemp map[int][]int = map[int][]int{}
-	var toHumidity map[int][]int = map[int][]int{}
-	var toLocation map[int][]int = map[int][]int{}
+func parseMaps() map[string]mapper {
+	maps := map[string]mapper{}
 
-	var s []int = []int{}
-	flag := ""
-	n := 0
-	for i, l := range lines {
-		if i == 0 {
-			s = getSeedValues(l)
+	var cur mapper
+	var name string
+	for _, line := range content[1:] {
+		if strings.Contains(line, "map:") {
+			names := strings.Split(line, " ")
+			names = strings.Split(names[0], "-")
+			name = names[0]
+
+			cur = mapper{to: names[2], rangers: []ranger{}}
 			continue
 		}
-		if l == "" {
-			flag = ""
-			n = 0
+		if line == "" {
 			continue
 		}
-
-		switch flag {
-		case "":
-			flag = getFlag(l)
-		case "to-soil":
-			toSoil[n] = addToMap(toSoil[n], l)
-			n++
-		case "to-fertilizer":
-			toFertilizer[n] = addToMap(toFertilizer[n], l)
-			n++
-		case "to-water":
-			toWater[n] = addToMap(toWater[n], l)
-			n++
-		case "to-light":
-			toLight[n] = addToMap(toLight[n], l)
-			n++
-		case "to-temp":
-			toTemp[n] = addToMap(toTemp[n], l)
-			n++
-		case "to-humidity":
-			toHumidity[n] = addToMap(toHumidity[n], l)
-			n++
-		case "to-location":
-			toLocation[n] = addToMap(toLocation[n], l)
-			n++
-		}
+		cur.rangers = append(cur.rangers, parseRange(line))
+		maps[name] = cur
 	}
-
-	return s, toSoil, toFertilizer, toWater,
-		toLight, toTemp, toHumidity, toLocation
+	return maps
 }
 
-func getSeedValues(l string) []int {
+func parseRange(line string) ranger {
+	vals := strings.Split(line, " ")
+	src, err := strconv.Atoi(vals[1])
+	if err != nil {
+		panic(err)
+	}
+	dest, err := strconv.Atoi(vals[0])
+	if err != nil {
+		panic(err)
+	}
+	length, err := strconv.Atoi(vals[2])
+	if err != nil {
+		panic(err)
+	}
+	return ranger{src: src, dest: dest, length: length}
+}
+
+func parseNegativeRange() map[string]mapper {
+	newMaps := maps
+	for name, m := range maps {
+		rangers := m.rangers
+		slices.SortFunc(rangers, func(a, b ranger) int {
+			return cmp.Compare(a.src, b.src)
+		})
+
+		start := 0
+		for i := 0; i < len(rangers); i++ {
+			r := rangers[i]
+			if rangers[i].src > start {
+				rangers = append(rangers, ranger{})
+				copy(rangers[i+1:], rangers[i:])
+				rangers[i] = ranger{src: start, dest: start, length: r.length}
+				i++
+			}
+			start = r.src + r.length
+		}
+		m.rangers = rangers
+		newMaps[name] = m
+	}
+	return newMaps
+}
+
+func cleanArray(arr []string) []string {
+	for i, v := range arr {
+		if v == "" {
+			arr = append(arr[:i], arr[i+1:]...)
+		}
+	}
+	return arr
+}
+
+func parseSeeds(vals []string) []int {
 	s := []int{}
-	l = strings.Split(l, ":")[1]
-	for _, val := range strings.Split(l, " ") {
-		if val == "" {
-			continue
-		}
-		num, err := strconv.Atoi(val)
+	for _, v := range vals {
+		seed, err := strconv.Atoi(v)
 		if err != nil {
 			panic(err)
 		}
-		s = append(s, num)
+		s = append(s, seed)
 	}
 	return s
 }
 
-func getRanges() map[int][]int {
-	ranges := map[int][]int{}
-
-	n := 0
-	for i := 0; i < len(seeds)-1; i += 2 {
-		ranges[n] = []int{seeds[i], seeds[i] + seeds[i+1] - 1}
-		n++
-	}
-	return ranges
-}
-
-func addToMap(m []int, l string) []int {
-	vals := strings.Split(l, " ")
-	for _, val := range vals {
-		v, err := strconv.Atoi(val)
+func parseSeedRanges(vals []string) []seeds {
+	s := []seeds{}
+	for i := 0; i < len(vals); i += 2 {
+		start, err := strconv.Atoi(vals[i])
 		if err != nil {
 			panic(err)
 		}
-		m = append(m, v)
+		length, err := strconv.Atoi(vals[i+1])
+		if err != nil {
+			panic(err)
+		}
+		s = append(s, seeds{start, length})
 	}
-	return m
+	return s
 }
 
-func getFlag(l string) string {
-	if strings.Contains(l, "to-soil") {
-		return "to-soil"
+func (m mapper) FindRange(val int) *ranger {
+	for _, r := range m.rangers {
+		if r.src <= val && r.src+r.length > val {
+			return &r
+		}
 	}
-	if strings.Contains(l, "to-fertilizer") {
-		return "to-fertilizer"
-	}
-	if strings.Contains(l, "to-water") {
-		return "to-water"
-	}
-	if strings.Contains(l, "to-light") {
-		return "to-light"
-	}
-	if strings.Contains(l, "to-temp") {
-		return "to-temp"
-	}
-	if strings.Contains(l, "to-humidity") {
-		return "to-humidity"
-	}
-	if strings.Contains(l, "to-location") {
-		return "to-location"
-	}
-	return ""
+	return nil
 }
