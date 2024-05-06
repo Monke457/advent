1a0194064d0c609d3688167595911ba1f1e968ba
Beth <beth.williams@bluewin.ch>
2024-05-06 09:25:46 +0200

    day 11 bleh

--- internal/day11/main.go
+++ internal/day11/main.go
@@ -17,6 +17,11 @@ func main() {
 		fmt.Println(d)
 	}
 
+	fmt.Println(solveFirstProblem(galaxies))
+	fmt.Println(solveSecondProblem(data, galaxies))
+}
+
+func solveFirstProblem(galaxies map[int][2]int) int {
 	sum := 0
 	for i := 1; i <= len(galaxies); i++ {
 		for j := i; j <= len(galaxies); j++ {
@@ -27,7 +32,40 @@ func main() {
 			sum += math.AbsDiff(galaxies[j][1], galaxies[i][1])
 		}
 	}
-	fmt.Println("Sum:", sum)
+	return sum
+}
+
+func solveSecondProblem(data []string, galaxies map[int][2]int) int {
+	sum := 0
+	for i := 1; i <= len(galaxies); i++ {
+		for j := i; j <= len(galaxies); j++ {
+			if i == j {
+				continue
+			}
+			a := offsetGalaxy(data, galaxies[i])
+			b := offsetGalaxy(data, galaxies[j])
+			fmt.Println(i, ":", a, j, ":", b)
+			sum += math.AbsDiff(a[0], b[0])
+			sum += math.AbsDiff(a[1], b[1])
+		}
+	}
+	return sum
+}
+
+func offsetGalaxy(data []string, g [2]int) [2]int {
+	result := [2]int{g[0], g[1]}
+
+	for i := 0; i < g[0]; i++ {
+		if data[i][0] == 42 {
+			result[0] -= 1
+			result[0] += 10
+		}
+	}
+	stars := strings.Count(data[0][:g[1]], "*")
+	result[1] -= stars
+	result[1] += stars * 10
+
+	return result
 }
 
 func parseData(lines []string) ([]string, map[int][2]int) {
