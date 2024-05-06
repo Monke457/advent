ee1a454635ad8746f44bbacaa3d9fd54f45a62b9
Beth <beth.williams@bluewin.ch>
2024-05-06 18:59:49 +0200

    day 11 part 2

--- internal/day11/main.go
+++ internal/day11/main.go
@@ -12,16 +12,19 @@ func main() {
 
 	data, galaxies := parseData(lines)
 
-	fmt.Println(galaxies)
-	for _, d := range data {
-		fmt.Println(d)
-	}
-
-	fmt.Println(solveFirstProblem(galaxies))
-	fmt.Println(solveSecondProblem(data, galaxies))
+	fmt.Println(solve(data, galaxies, 2))
+	fmt.Println(solve(data, galaxies, 1000000))
 }
 
-func solveFirstProblem(galaxies map[int][2]int) int {
+func solve(data []string, galaxies map[int][2]int, offset int) int {
+	newGalaxies := map[int][2]int{}
+	for i := range galaxies {
+		newGalaxies[i] = offsetGalaxy(data, galaxies[i], offset)
+	}
+	return calculateDistance(newGalaxies)
+}
+
+func calculateDistance(galaxies map[int][2]int) int {
 	sum := 0
 	for i := 1; i <= len(galaxies); i++ {
 		for j := i; j <= len(galaxies); j++ {
@@ -35,35 +38,18 @@ func solveFirstProblem(galaxies map[int][2]int) int {
 	return sum
 }
 
-func solveSecondProblem(data []string, galaxies map[int][2]int) int {
-	sum := 0
-	for i := 1; i <= len(galaxies); i++ {
-		for j := i; j <= len(galaxies); j++ {
-			if i == j {
-				continue
-			}
-			a := offsetGalaxy(data, galaxies[i])
-			b := offsetGalaxy(data, galaxies[j])
-			fmt.Println(i, ":", a, j, ":", b)
-			sum += math.AbsDiff(a[0], b[0])
-			sum += math.AbsDiff(a[1], b[1])
-		}
-	}
-	return sum
-}
-
-func offsetGalaxy(data []string, g [2]int) [2]int {
+func offsetGalaxy(data []string, g [2]int, offset int) [2]int {
 	result := [2]int{g[0], g[1]}
 
 	for i := 0; i < g[0]; i++ {
 		if data[i][0] == 42 {
 			result[0] -= 1
-			result[0] += 10
+			result[0] += offset
 		}
 	}
 	stars := strings.Count(data[0][:g[1]], "*")
 	result[1] -= stars
-	result[1] += stars * 10
+	result[1] += stars * offset
 
 	return result
 }
@@ -91,21 +77,20 @@ func parseData(lines []string) ([]string, map[int][2]int) {
 				s = fmt.Sprintf("%d", count)
 				galaxies[count] = [2]int{row, column}
 			}
-			sb.WriteString(s)
 			column++
 			if col[j] {
+				sb.WriteString(s)
 				continue
 			}
-			sb.WriteString("*")
-			column++
+			s = "*"
+			sb.WriteString(s)
 		}
-		data = append(data, sb.String())
 		row++
 		if strings.Contains(l, "#") {
+			data = append(data, sb.String())
 			continue
 		}
 		data = append(data, strings.Repeat("*", len(sb.String())))
-		row++
 	}
 	return data, galaxies
 }
