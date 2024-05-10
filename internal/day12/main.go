4dead59567631edb2d8ed03fe8037ac6b714e476
Beth <beth.williams@bluewin.ch>
2024-05-10 10:00:00 +0200

    stolen day 12 :(

--- internal/day12/main.go
+++ internal/day12/main.go
@@ -3,63 +3,117 @@ package main
 import (
 	"advent/internal/pkg/reader"
 	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
 type config struct {
-	springs      string
-	broken       []int
-	arrangements []string
+	springs []byte
+	broken  []int
+}
+
+func (c config) Extend() config {
+	nc := config{}
+	for range 4 {
+		nc.springs = append(nc.springs, c.springs[:]...)
+		nc.springs = append(nc.springs, '?')
+		nc.broken = append(nc.broken, c.broken[:]...)
+	}
+	nc.springs = append(nc.springs, c.springs[:]...)
+	nc.broken = append(nc.broken, c.broken[:]...)
+	return nc
+}
+
+func slicesMap[T, U any](t []T, f func(T) U) []U {
+	us := make([]U, len(t))
+	for i := range t {
+		us[i] = f(t[i])
+	}
+	return us
+}
+
+func atoi(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func mapsClear[M ~map[K]V, K comparable, V any](m M) {
+	for k := range m {
+		delete(m, k)
+	}
 }
 
 func main() {
 	lines := reader.FileToArray("data/day12.txt")
 	configs := parseConfig(lines)
 
-	for _, c := range configs {
-		fmt.Println("springs:", c.springs, " broken: ", c.broken)
-	}
-
-	fmt.Println(solveFirstProblem(configs))
+	fmt.Println(solveProblem(configs))
+	fmt.Println(solveSecondProblem(configs))
 }
 
-func solveFirstProblem(configs []config) int {
+func solveProblem(configs []config) int {
 	sum := 0
-	for _, config := range configs {
-		sum += config.GetArrangements()
+	for _, c := range configs {
+		sum += countPossible(c.springs, c.broken)
 	}
 	return sum
 }
 
-func (c config) GetArrangements() int {
-	arrs := []string{}
-	pieces := strings.Split(c.springs, ".")
-	for _, i := range c.broken {
-		fmt.Println(i, "looking for pattern in", pieces)
+func solveSecondProblem(configs []config) int {
+	nc := []config{}
+	for _, c := range configs {
+		nc = append(nc, c.Extend())
 	}
-	return len(arrs)
+	return solveProblem(nc)
+}
+
+func countPossible(s []byte, b []int) int {
+	poss := 0
+
+	cstates := map[[3]int]int{{0, 0, 0}: 1}
+	nstates := map[[3]int]int{}
+
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		for state, num := range cstates {
+			ci, cc, expdot := state[0], state[1], state[2]
+			switch {
+			case (c == '#' || c == '?') && ci < len(b) && expdot == 0:
+				if c == '?' && cc == 0 {
+					nstates[[3]int{ci, cc, expdot}] += num
+				}
+				cc++
+				if cc == b[ci] {
+					ci, cc, expdot = ci+1, 0, 1
+				}
+				nstates[[3]int{ci, cc, expdot}] += num
+			case (c == '.' || c == '?') && cc == 0:
+				expdot = 0
+				nstates[[3]int{ci, cc, expdot}] += num
+			}
+		}
+		cstates, nstates = nstates, cstates
+		mapsClear(nstates)
+	}
+
+	for s, v := range cstates {
+		if s[0] == len(b) {
+			poss += v
+		}
+	}
+
+	fmt.Printf("springs:%s, broken:%d, arrangements:%d\n", s, b, poss)
+	return poss
 }
 
 func parseConfig(lines []string) []config {
 	configs := []config{}
-	re := regexp.MustCompile(`\.+`)
 	for _, l := range lines {
-		line := strings.Split(l, " ")
-		springs := strings.TrimSpace(line[0])
-		springs = re.ReplaceAllString(springs, ".")
+		springs, broken, _ := strings.Cut(l, " ")
+		s := []byte(springs)
 
-		broken := strings.Split(line[1], ",")
-		brokenInt := []int{}
-		for _, b := range broken {
-			bInt, err := strconv.Atoi(b)
-			if err != nil {
-				panic(err)
-			}
-			brokenInt = append(brokenInt, bInt)
-		}
-		configs = append(configs, config{springs: springs, broken: brokenInt})
+		b := slicesMap(strings.Split(broken, ","), atoi)
+		configs = append(configs, config{springs: s, broken: b})
 	}
 	return configs
 }
