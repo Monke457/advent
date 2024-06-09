576016e2581e77e6642090f31bfe407b779857d4
Beth <beth.williams@bluewin.ch>
2024-06-09 11:41:28 +0200

    2016 day 9 part 2

--- internal/2016/day9/main.go
+++ internal/2016/day9/main.go
@@ -10,31 +10,41 @@ import (
 
 func main() {
 	data := reader.FileToString("data/2016/day9.txt")
-	res := findCompLen(data)
-	fmt.Println("First:", res)
+	res1 := findCompLenRec(data)
+	fmt.Println("First:", res1)
+	res2 := findCompLenRec(data)
+	fmt.Println("Second:", res2)
+}
+
+func findCompLenRec(data string) int {
+	re := regexp.MustCompile(`\(\d*x\d*\)`)
+	match := re.FindStringIndex(data)
+
+	if match == nil {
+		return len(data)
+	}
+
+	r := match[0]
+	l, c := parseCompLen(data[match[0]:match[1]])
+	r += c * findCompLenRec(data[match[1]:match[1]+l])
+	return r + findCompLenRec(data[match[1]+l:])
 }
 
 func findCompLen(data string) int {
 	re := regexp.MustCompile(`\(\d*x\d*\)`)
-	result := strings.Builder{}
 	match := re.FindStringIndex(data)
 
+	r := 0
 	for match != nil {
-		result.WriteString(data[:match[0]])
-		
+		r += match[0]
 		l, c := parseCompLen(data[match[0]:match[1]])
-
-		for range c {
-			result.WriteString(data[match[1]:match[1]+l])
-		}
-
+		r += c * l
 		data = data[match[1]+l:]
 		match = re.FindStringIndex(data)
 	}
 
-	result.WriteString(data)
-	
-	return result.Len() 
+	r += len(data)
+	return r
 }
 
 func parseCompLen(str string) (int, int) {
--- internal/2016/day9/main_test.go
+++ internal/2016/day9/main_test.go
@@ -49,3 +49,37 @@ func TestFindCompLen(t *testing.T) {
 		t.Log("Passed", data[5], ":", l6)
 	}
 }
+
+func TestFindCompLenRec(t *testing.T) {
+	data := []string{
+		"(3x3)XYZ",
+		"X(8x2)(3x3)ABCY",
+		"(27x12)(20x12)(13x14)(7x10)(1x12)A",
+		"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN",
+	}
+
+	l1 := findCompLenRec(data[0])
+	if l1 != 9 {
+		t.Errorf("Decompression failed %s, expected 9 got %d", data[0], l1)
+	} else {
+		t.Log("Passed", data[0], ":", l1)
+	}
+	l2 := findCompLenRec(data[1])
+	if l2 != 20 {
+		t.Errorf("Decompression failed, %s expected 20 got %d", data[1], l2)
+	} else {
+		t.Log("Passed", data[1], ":", l2)
+	}
+	l3 := findCompLenRec(data[2])
+	if l3 != 241920 {
+		t.Errorf("Decompression failed, %s expected 241920 got %d", data[2], l3)
+	} else {
+		t.Log("Passed", data[2], ":", l3)
+	}
+	l4 := findCompLenRec(data[3])
+	if l4 != 445 {
+		t.Errorf("Decompression failed, %s expected 445 got %d", data[3], l4)
+	} else {
+		t.Log("Passed", data[3], ":", l4)
+	}
+}
