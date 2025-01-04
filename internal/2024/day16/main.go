c0ccd7ea057060c69620d43d273669f4bac5674c
Beth <beth.williams@bluewin.ch>
2025-01-04 18:39:14 +0100

    2024 day 16 part 2

--- internal/2024/day16/main.go
+++ internal/2024/day16/main.go
@@ -15,106 +15,159 @@ const (
 	WEST
 )
 
+type state struct {
+	pos [2]int
+	facing direction
+}
+
 func main() {
 	data := reader.FileTo2DArray("data/2024/day16.txt")
 
-	path := walk(data, EAST)
-	sum := sumPath(path, EAST)
-	
-	draw(data, path)
-	fmt.Println("Best score:", sum)
+	paths := aStarSearch(data, math.MaxInt)
+	best := sumPath(paths[0], EAST)
+	reduced := reducePaths(paths)
+
+	fmt.Println("Bet score possible:", best)
+	fmt.Println("Tiles that are part of at least one winning path:", len(reduced))
 }
 
-func draw(data [][]rune, path [][2]int) {
-	for y := range data {
-		for x := range data[y] {
-			if slices.Contains(path, [2]int{x, y}) {
-				fmt.Printf("+")
-			} else {
-				fmt.Printf("%c", data[y][x])
-			}
+func reducePaths(paths [][][2]int) map[[2]int]bool {
+	res := map[[2]int]bool{}
+	for _, path := range paths {
+		for _, tile := range path {
+			res[tile] = true
 		}
-		fmt.Println()
 	}
+	return res
 }
 
-func walk(data [][]rune, facing direction) [][2]int {
-	src := findRune(data, 'S')
+func aStarSearch(data [][]rune, best int) [][][2]int {
+	src := state{findRune(data, 'S'), EAST}
 	dest := findRune(data, 'E')
-	visited := map[[2]int]bool{}
-	costs := map[[2]int]int{src:0}
-	queue := map[[2]int]int{src:0}
-	predecessors := map[[2]int]*[2]int{src:nil}
+	queue := map[state]int{src:0}
+	visited := map[state]bool{}
+	destStates := []state{}
+	predecessors := map[state]map[state]bool{src:{}}
 
-	var next [2]int
-	for {
-		//while queue not empty 
-		if len(queue) == 0 {
-			break
-		}
+	var next state
+	var cost int
 
-		//pop next highest priority node
-		queue, next = pop(queue)
+	for len(queue) > 0 {
+		queue, next, cost = pop(queue)
 
-		if next == dest {
-			break
-		}
-
-		//do not visit same node twice
-		if visited[next] {
+		if cost > best {
 			continue
 		}
 
-		//mark node as visited
-		visited[next] = true
-
-		//set current direction
-		if predecessors[next] != nil {
-			facing = getDirection(*predecessors[next], next)
+		if next.pos == dest {
+			if cost < best {
+				best = cost
+				clear(predecessors)
+				queue[src] = 0
+			}
+			destStates = append(destStates, next)
+			clear(visited)
+			continue
 		}
 
-		//calculate costs of neighbouring nodes
+		if visited[next] {
+			continue
+		}
+		visited[next] = true
+
 		for _, n := range getNeighbours(data, next) {
-			if data[n[1]][n[0]] == '#' {
+			if visited[n] {
 				continue
 			}
 
-			currCost := costs[next] + 1
-			if getDirection(next, n) != facing {
+			currCost := cost + 1
+			if n.facing != next.facing {
 				currCost += 1000
 			}
+			if currCost > best {
+				continue
+			} 
 
-			//if we don't have cost for this node 
-			//or it's less than what we have, add to queue
-			if prev, ok := costs[n]; !ok || currCost < prev {
-				costs[n] = currCost
-				//not the best...
-				queue[n] = costs[n] + heuristic(n, dest)
-				pred := [2]int{next[0], next[1]}
-				predecessors[n] = &pred
+			if prev, ok := queue[n]; !ok || currCost < prev {
+				queue[n] = currCost
 			}
+			if _, ok := predecessors[n]; !ok {
+				predecessors[n] = map[state]bool{}
+			}
+			predecessors[n][next] = true
 		}
 	}
 
-	return makePath(predecessors, dest)
+	finalPaths := getFinalPaths(predecessors, destStates, src.pos, best)
+	return finalPaths
 }
 
-func makePath(predecessors map[[2]int]*[2]int, dest [2]int) [][2]int {
-	path := [][2]int{dest}
-	pred := predecessors[dest]
-	for pred != nil {
-		path = append(path, *pred)
-		pred = predecessors[*pred]
+func getFinalPaths(preds map[state]map[state]bool, targetStates []state, start [2]int, best int) [][][2]int {
+	paths := [][][2]int{}
+	for _, target := range targetStates {
+		paths = append(paths, backtrack(preds, map[[2]int]bool{}, [][2]int{target.pos}, target, start, 0, best)...)
 	}
-	slices.Reverse(path)
-	return path
+	return paths 
 }
 
+func backtrack(preds map[state]map[state]bool, visited map[[2]int]bool, path [][2]int, curr state, start [2]int, cost, best int) [][][2]int {
+	if curr.pos == start {
+		return [][][2]int{path}
+	}
+	allPaths := [][][2]int{}
+	visited[curr.pos] = true
+	for prev := range preds[curr] {
+		if visited[prev.pos] {
+			continue
+		}
 
-func heuristic(node, dest [2]int) int {
-	a := math.Abs(float64(node[0]-dest[0]))
-	b := math.Abs(float64(node[1]-dest[1])) 
-	return int(a + b) * int(a + b)
+		moveCost := 1
+		if prev.facing != curr.facing {
+			moveCost += 1000
+		}
+		if cost + moveCost > best {
+			continue
+		}
+
+		path = append([][2]int{prev.pos}, path...)
+		allPaths = append(allPaths, backtrack(preds, visited, path, prev, start, cost+moveCost, best)...)
+		path = path[1:]
+	}
+	delete(visited, curr.pos)
+	return allPaths
+}
+
+var dirs = map[direction][2]int{NORTH:{0,-1},SOUTH:{0,1},EAST:{1,0},WEST:{-1, 0}}
+
+func getNeighbours(data [][]rune, s state) []state {
+	neighbours := []state{}
+	for facing, coords := range dirs {
+		x, y := s.pos[0]+coords[0], s.pos[1]+coords[1]
+		if y < 0 || y >= len(data) {
+			continue
+		}
+		if x < 0 || x >= len(data[y]) {
+			continue
+		}
+		if data[y][x] == '#' {
+			continue
+		}
+		pos := [2]int{x, y}
+		neighbours = append(neighbours, state{pos, facing})
+	}
+	return neighbours
+}
+
+func pop(queue map[state]int) (map[state]int, state, int) {
+	s, prio := state{}, math.MaxInt
+	for k, v := range queue {
+		if v < prio {
+			s = k
+			prio = v
+		}
+	}
+	delete(queue, s)
+	return queue, s, prio
 }
 
 func getDirection(curr, next [2]int) direction {
@@ -130,36 +183,7 @@ func getDirection(curr, next [2]int) direction {
 	if curr[0]-1 == next[0] {
 		return WEST
 	}
-	panic("Fix your skill issues, Blizzard!")
-}
-
-var dirs = [4][2]int{{0,-1},{0,1},{1,0},{-1, 0}}
-
-func getNeighbours(data [][]rune, node [2]int) [][2]int {
-	neighbours := [][2]int{}
-	for _, dir := range dirs {
-		x, y := node[0]+dir[0], node[1]+dir[1]
-		if y < 0 || y >= len(data) {
-			continue
-		}
-		if x < 0 || x >= len(data[y]) {
-			continue
-		}
-		neighbours = append(neighbours, [2]int{x, y})
-	}
-	return neighbours
-}
-
-func pop(queue map[[2]int]int) (map[[2]int]int, [2]int) {
-	node, prio := [2]int{-1, -1}, math.MaxInt
-	for k, v := range queue {
-		if v < prio {
-			node = k
-			prio = v
-		}
-	}
-	delete(queue, node)
-	return queue, node
+	panic(fmt.Errorf("Fix your skill issues, Blizzard! %d %d", curr, next))
 }
 
 func sumPath(path [][2]int, facing direction) int {
@@ -185,3 +209,17 @@ func findRune(data [][]rune, r rune) [2]int {
 	}
 	panic("Could not find rune position")
 }
+
+func draw(data [][]rune, path [][2]int) {
+	for y := range data {
+		for x := range data[y] {
+			if slices.Contains(path, [2]int{x, y}) {
+				fmt.Printf("0")
+			} else {
+				fmt.Printf("%c", data[y][x])
+			}
+		}
+		fmt.Println()
+	}
+}
+
