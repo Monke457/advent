67dc260b3be4ea83036d4e1bef0a9ed477947735
Beth <beth.williams@bluewin.ch>
2025-03-05 18:50:05 +0100

    ez

---
+++ internal/2015/day1/zig/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
+
+pub fn main() !void {
+    const file = try std.fs.cwd().openFile("data/2015/day1.txt", .{});
+    defer file.close();
+    const reader = file.reader();
+
+    var pos: i32 = 0;
+    var basement: i32 = 0;
+    var i: i32 = 0;
+
+    while (reader.readByte()) |byte| {
+        i = i + 1;
+        if (byte == 10) break;
+        if (byte == 40) pos = pos + 1;
+        if (byte == 41) pos = pos - 1;
+        if (pos < 0 and basement == 0) {
+            basement = i;
+        }
+    } else |err| {
+        print("Error while reading the file: {}\n", .{err});
+    }
+
+    print("final position: {d}\n", .{pos});
+    print("enters basement: {d}\n", .{basement});
+}
